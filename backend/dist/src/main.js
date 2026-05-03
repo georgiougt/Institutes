c6dc3f8e0d2edf681e632958bfa17ef1d716501f
Georg <georgiougt94@icloud.com>
2026-05-03 22:40:48 +0300

    debug: add startup logs

--- backend/dist/src/main.js
+++ backend/dist/src/main.js
@@ -5,6 +5,7 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
+    console.log('🚀 SERVER ATTEMPTING TO START...');
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
         origin: true,
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  console.log('🚀 SERVER ATTEMPTING TO START...');
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
